c0bc8649608ca79c3e3969d7745afc9706db3de9
farhan <farhanfarhu60@gmail.com>
2024-04-05 17:23:09 +0530

    Updated new second

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_app/application/bloc/Bottom/bottom_nav_bloc.dart';
@@ -30,21 +31,38 @@ import 'package:travel_app/presentation/commentScreens/startScreen.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid
-      ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-          apiKey: "AIzaSyDuHbcxY5PxMOq6nJ-VcAkLxp4LjPTCaYI",
+
+  // Initialize Firebase based on platform
+  if (Platform.isAndroid) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyDuHbcxY5PxMOq6nJ-VcAkLxp4LjPTCaYI",
+      projectId: "travel-8c5ff",
+      messagingSenderId: "750098882666",
+      appId: "1:750098882666:android:bc0eaa696a0a8573916a13",
+      storageBucket: 'travel-8c5ff.appspot.com',
+    ));
+  } else if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyB0g0UoVfj9I2g8xv2Un5uXTBcPx-RccbQ",
+          authDomain: "travel-8c5ff.firebaseapp.com",
+          databaseURL: "https://travel-8c5ff-default-rtdb.firebaseio.com",
           projectId: "travel-8c5ff",
+          storageBucket: "travel-8c5ff.appspot.com",
           messagingSenderId: "750098882666",
-          appId: "1:750098882666:android:bc0eaa696a0a8573916a13",
-          storageBucket: 'travel-8c5ff.appspot.com',
-        ))
-      : await Firebase.initializeApp();
+          appId: "1:750098882666:web:7f846612087066a7916a13",
+          measurementId: "G-1RLMNS6LPG"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(MaterialsScreen());
 }
 
 class MaterialsScreen extends StatefulWidget {
-  const MaterialsScreen({super.key});
+  const MaterialsScreen({Key? key}) : super(key: key);
 
   @override
   State<MaterialsScreen> createState() => _MaterialsScreenState();
@@ -52,56 +70,48 @@ class MaterialsScreen extends StatefulWidget {
 
 class _MaterialsScreenState extends State<MaterialsScreen> {
   @override
-  @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-        providers: [
-          BlocProvider<CountryBloc>(create: (context) => CountryBloc()),
-          BlocProvider<BottomNavBloc>(create: (context) => BottomNavBloc()),
-          BlocProvider<ImgBlocBloc>(
-              create: (context) => ImgBlocBloc(ImagePickerServices())),
-          BlocProvider<UserBloc>(create: (context) => UserBloc(UserProfile())),
-          BlocProvider<PackageBloc>(
-              create: (context) => PackageBloc(PackageSearvicesRepo())),
-          BlocProvider<AgencyBloc>(
-              create: (context) => AgencyBloc(AgencyProfoRepo())),
-          BlocProvider<AgencyIndvBloc>(
-              create: (context) => AgencyIndvBloc(AgencyindvRepo())),
-          BlocProvider<TodoBloc>(
-              create: (context) => TodoBloc(BookingInternational())),
-          BlocProvider<BookedBloc>(
-              create: (context) => BookedBloc(BookedRepo())),
-          BlocProvider<PackageUidBloc>(
-              create: (context) => PackageUidBloc(IndivtualPackageRepo())),
-          BlocProvider<ChatRoomBloc>(
-              create: (context) => ChatRoomBloc(MessageRepo())),
-          BlocProvider<bookedHistoryBloc>(
-              create: (context) => bookedHistoryBloc(BookedRepo())),
-          BlocProvider<FavoretBloc>(create: (context) => FavoretBloc(Wish())),
-            BlocProvider<FilterBloc>(create: (context) => FilterBloc()),
-        ],
-        child: MaterialApp(
-          theme: ThemeData.dark(
-            useMaterial3: true,
+      providers: [
+        BlocProvider<CountryBloc>(create: (context) => CountryBloc()),
+        BlocProvider<BottomNavBloc>(create: (context) => BottomNavBloc()),
+        BlocProvider<ImgBlocBloc>(
+            create: (context) => ImgBlocBloc(ImagePickerServices())),
+        BlocProvider<UserBloc>(create: (context) => UserBloc(UserProfile())),
+        BlocProvider<PackageBloc>(
+            create: (context) => PackageBloc(PackageSearvicesRepo())),
+        BlocProvider<AgencyBloc>(
+            create: (context) => AgencyBloc(AgencyProfoRepo())),
+        BlocProvider<AgencyIndvBloc>(
+            create: (context) => AgencyIndvBloc(AgencyindvRepo())),
+        BlocProvider<TodoBloc>(
+            create: (context) => TodoBloc(BookingInternational())),
+        BlocProvider<BookedBloc>(create: (context) => BookedBloc(BookedRepo())),
+        BlocProvider<PackageUidBloc>(
+            create: (context) => PackageUidBloc(IndivtualPackageRepo())),
+        BlocProvider<ChatRoomBloc>(
+            create: (context) => ChatRoomBloc(MessageRepo())),
+        BlocProvider<bookedHistoryBloc>(
+            create: (context) => bookedHistoryBloc(BookedRepo())),
+        BlocProvider<FavoretBloc>(create: (context) => FavoretBloc(Wish())),
+        BlocProvider<FilterBloc>(create: (context) => FilterBloc()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark(
+          useMaterial3: true,
+        ),
+        darkTheme: ThemeData(
+          fontFamily: 'Poppins',
+          primaryColor: const Color(0xFF0A1529),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFFFFB325),
+            secondary: const Color(0xFFFFB325),
           ),
-          darkTheme: ThemeData(
-            fontFamily: 'Poppins',
-            primaryColor: const Color(0xFF0A1529),
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: const Color(0xFFFFB325),
-                secondary: const Color(0xFFFFB325)),
-            useMaterial3: true,
-          ),
-          // theme: ThemeData(
-          //   fontFamily: 'Poppins',
-          //   primaryColor: const Color(0xFF0A1529),
-          //   colorScheme: ColorScheme.fromSeed(
-          //       seedColor: const Color(0xFFFFB325),
-          //       secondary: const Color(0xFFFFB325)),
-          //   useMaterial3: true,
-          // ),
-          debugShowCheckedModeBanner: false,
-          home: StartScreen(),
-        ));
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: StartScreen(),
+      ),
+    );
   }
 }
--- lib/presentation/userScreen/bookedpackage/bookedpackagedetials/bookingPackageDetials.dart
+++ lib/presentation/userScreen/bookedpackage/bookedpackagedetials/bookingPackageDetials.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
